e431023b7927baf1459bd6b6ed12a3cb8dfc6ab1
ChesnAnastasia <chesnv_nastya@mail.ru>
2018-03-11 19:42:23 +0300

    add dom.js

--- task4/index.js
+++ task4/index.js
@@ -1,152 +1,8 @@
-var photoPosts = [
-    {
-        id: '1',
-        description: 'Czech Republic',
-        createdAt: new Date(2014, 10, 21, 30, 15),
-        author: 'ChesnAnastasia',
-        photoLink: '1.jpg',
-        tags: ['#beauty', '#travels', '#hobby'],
-        likes: ['Masha', 'Kate']
-    },
-    {
-        id: '2',
-        description: 'Russia',
-        createdAt: new Date(2015, 04, 28, 23, 18),
-        author: 'Masha',
-        photoLink: '2.jpg',
-        tags: ['#beauty', '#lovenature', '#hobby'],
-        likes: ['ChesnAnastasia', 'Kate', 'Dasha']
-    },
-    {
-        id: '3',
-        description: 'Czech Republic',
-        createdAt: new Date(2016, 12, 21, 23, 14),
-        author: 'Dasha',
-        photoLink: '3.jpg',
-        tags: ['#beauty', '#lovenature'],
-        likes: ['Masha', 'Kate']
-    },
-    {
-        id: '4',
-        description: 'Hungary',
-        createdAt: new Date(2015, 04, 28, 23, 18),
-        author: 'Masha',
-        photoLink: '4.jpg',
-        tags: ['#pleasure', '#travels', '#hobby'],
-        likes: ['ChesnAnastasia', 'Kate']
-    },
-    {
-        id: '5',
-        description: 'Hungary',
-        createdAt: new Date(2016, 12, 21, 23, 14),
-        author: 'ChesnAnastasia',
-        photoLink: '5.jpg',
-        tags: ['#beauty', '#pleasure'],
-        likes: ['Dasha', 'Kate', 'Masha']
-    },
-    {
-        id: '6',
-        description: 'rest',
-        createdAt: new Date(2014, 10, 21, 30, 15),
-        author: 'ChesnAnastasia',
-        photoLink: '1.jpg',
-        tags: ['#beauty', '#hobby'],
-        likes: ['Masha', 'Kate']
-    },
-    {
-        id: '7',
-        description: 'Russia',
-        createdAt: new Date(2015, 04, 28, 23, 18),
-        author: 'Masha',
-        photoLink: '2.jpg',
-        tags: ['#beauty', '#lovenature', '#hobby'],
-        likes: ['ChesnAnastasia', 'Kate', 'Dasha']
-    },
-    {
-        id: '8',
-        description: 'Czech Republic',
-        createdAt: new Date(2015, 12, 21, 23, 14),
-        author: 'Dasha',
-        photoLink: '3.jpg',
-        tags: ['#beauty', '#lovenature'],
-        likes: ['Masha', 'Kate']
-    },
-    {
-        id: '9',
-        description: 'Hungary',
-        createdAt: new Date(2015, 04, 15, 23, 18),
-        author: 'Kate',
-        photoLink: '4.jpg',
-        tags: ['#pleasure', '#travels', '#hobby'],
-        likes: ['ChesnAnastasia', 'Kate']
-    },
-    {
-        id: '10',
-        description: 'Hungary',
-        createdAt: new Date(2016, 12, 21, 23, 14),
-        author: 'ChesnAnastasia',
-        photoLink: '5.jpg',
-        tags: ['#beauty', '#pleasure'],
-        likes: ['Dasha', 'Kate', 'Masha']
-    }
-];
+var photoPosts = [];
 
-var post1 = {
-    id: '11',
-    description: 'hello world',
-    createdAt: new Date(2018, 02, 24, 18, 45),
-    author: 'Kate',
-    photoLink: 'photo link',
-    tags: ['#beauty', '#health'],
-    likes: ['Masha']
-};
-
-var editPost2 = {
-    id: '46',
-    description: 'changed',
-    createdAt: new Date(2018, 02, 24, 18, 45),
-    author: 'Kate',
-    photoLink: 'photo link1',
-    tags: ['#beauty', '#health'],
-    likes: ['Masha']
-};
-
-var forValid1 = {
-    id: '46',
-    description: 308,
-    createdAt: new Date(2018, 02, 24, 18, 45),
-    author: 'Kate',
-    photoLink: [3, 4],
-    tags: ['#beauty', '#health'],
-    likes: ['Masha']
-};
-var forValid2 = {
-    id: '46',
-    description: 'dfghj',
-    createdAt: new Date(2018, 02, 24, 18, 45),
-    author: 'Kate',
-    photoLink: [3, 4],
-    tags: ['#beauty', '#health'],
-    likes: ['Masha']
-};
-
-var filter1 = {
-    author: 'Masha'
-}
-var filter2 = {
-    author: 'ChesnAnastasia',
-    createdAt: new Date(2014, 10, 21, 30, 15)
-}
-var filter3 = {
-    author: 'ChesnAnastasia',
-    createdAt: new Date(2014, 10, 21, 30, 15),
-    tags: ['#travels']
-}
-
-let module = (function () {
+let moduleF = (function () {
 
     photoPosts.sort(compareByDate);
-
     function compareByDate(photoPostA, photoPostB) {
         return Date.parse(photoPostB.createdAt) - Date.parse(photoPostA.createdAt);
     }
@@ -163,20 +19,19 @@ let module = (function () {
         }
         return false;
     }
-
-    let validatePhotoPost = function (photoPost){
-        if (photoPost.id && typeof(photoPost.id) === 'string' 
-            && photoPost.description && typeof(photoPost.description) === 'string' && photoPost.description.length <= 300
+    let validatePhotoPost = function (photoPost) {
+        if (photoPost.id && typeof (photoPost.id) === 'string'
+            && photoPost.description && typeof (photoPost.description) === 'string' && photoPost.description.length <= 300
             && photoPost.createdAt && photoPost.createdAt instanceof Date
-            && photoPost.author && typeof(photoPost.author) === 'string' && photoPost.author !== ''
-            && photoPost.photoLink && typeof(photoPost.photoLink) === 'string'  && photoPost.photoLink !== ''
-            && (typeof(photoPost.tags) === 'undefined' || (validArr(photoPost.tags) && photoPost.tags.length <= 30))
-            && (typeof(photoPost.likes) === 'undefined' || validArr(photoPost.likes))) return true;
+            && photoPost.author && typeof (photoPost.author) === 'string' && photoPost.author !== ''
+            && photoPost.photoLink && typeof (photoPost.photoLink) === 'string' && photoPost.photoLink !== ''
+            && (typeof (photoPost.tags) === 'undefined' || (validArr(photoPost.tags) && photoPost.tags.length <= 30))
+            && (typeof (photoPost.likes) === 'undefined' || validArr(photoPost.likes))) return true;
         else return false;
     }
 
     let addPhotoPost = function (photoPost) {
-        if (validatePhotoPost(photoPost)){
+        if (validatePhotoPost(photoPost)) {
             photoPosts.push(photoPost);
             photoPosts.sort(compareByDate);
             return true;
@@ -184,13 +39,12 @@ let module = (function () {
         return false;
     }
 
-    function validateFilter(filterConfig){
+    function validateFilter(filterConfig) {
         if ((!filterConfig.author || (filterConfig.author && typeof (filterConfig.author) === 'string' && filterConfig.author !== ''))
             && (typeof (filterConfig.tags) === 'undefined' || (Array.isArray(filterConfig.tags) && filterConfig.tags.length <= 30))
             && (!filterConfig.createdAt || (filterConfig.createdAt && filterConfig.createdAt instanceof Date))) return true;
         else return false;
     }
-
     let getPhotoPosts = function (skip, top, filterConfig) {
         let posts = photoPosts;
         if (filterConfig && validateFilter(filterConfig)) {
@@ -228,10 +82,9 @@ let module = (function () {
             && (typeof (photoPost.likes) === 'undefined' || validArr(photoPost.likes))) return true;
         else return false;
     }
-
     let editPhotoPost = function (id, photoPost) {
         let editPost = this.getPhotoPost(id);
-        if (typeof(editPost) !== 'undefined' && validateForEditPost(photoPost)){
+        if (typeof (editPost) !== 'undefined' && validateForEditPost(photoPost)) {
             if (photoPost.description)
                 editPost.description = photoPost.description;
             if (photoPost.tags)
@@ -262,49 +115,3 @@ let module = (function () {
     }
 
 })();
-
-console.log(photoPosts);
-
-console.log("editPhotoPost('6', { description: 'a' })");
-console.log(module.editPhotoPost('6', { description: 'a' }));
-
-console.log(module.getPhotoPost('3'));
-console.log("getPhotoPost(3)");
-console.log(module.getPhotoPost(3));
-
-console.log("addPhotoPost(forValid1)");
-console.log(module.addPhotoPost(forValid1));
-console.log("addPhotoPost(forValid2)");
-console.log(module.addPhotoPost(forValid2));
-console.log("addPhotoPost(post1)");
-console.log(module.addPhotoPost(post1));
-console.log(photoPosts);
-
-console.log("editPhotoPost('51', { description: 'a' })");
-console.log(module.editPhotoPost('51', { description: 'a' }));
-console.log("editPhotoPost('6', { description: 'a' })");
-console.log(module.editPhotoPost('6', { description: 'a' }));
-console.log("editPhotoPost('6', forValid1)");
-console.log(module.editPhotoPost('6', forValid1));
-console.log("editPhotoPost('6', forValid2)");
-console.log(module.editPhotoPost('6', forValid2));
-console.log("editPhotoPost('6', editPost2)");
-console.log(module.editPhotoPost('6', editPost2));
-console.log(photoPosts);
-
-console.log("removePhotoPost(4)");
-console.log(module.removePhotoPost(4));
-console.log("removePhotoPost([4, 3])");
-console.log(module.removePhotoPost([4, 3]));
-console.log("removePhotoPost('4')");
-console.log(module.removePhotoPost('4'));
-console.log(photoPosts);
-
-console.log("getPhotoPosts(0, 5)");
-console.log(module.getPhotoPosts(0, 5));
-console.log("getPhotoPosts(0, 5, filter1)");
-console.log(module.getPhotoPosts(0, 5, filter1));
-console.log("getPhotoPosts(0, 5, filter2)");
-console.log(module.getPhotoPosts(0, 5, filter2));
-console.log("getPhotoPosts(0, 5, filter3)");
-console.log(module.getPhotoPosts(0, 5, filter3));
---
+++ task5/dom.js
@@ -0,0 +1,167 @@
+var post1 = {
+    id: '1',
+    description: 'Austria, Wien',
+    createdAt: new Date(2017, 03, 13, 16, 51),
+    author: 'Mary',
+    photoLink: '5.jpg',
+    likes: ['ChesnAnastasia', 'Kate'],
+    tags: ['#travel']
+}
+var post2 = {
+    id: '2',
+    description: 'Russia, Sankt-Peterburg',
+    createdAt: new Date(2017, 06, 07, 12, 49),
+    author: 'ChesnAnastasia',
+    photoLink: '4.jpg',
+    likes: ['Mary', 'Kate'],
+    tags: ['#travel', '#beauty']
+}
+var post3 = {
+    id: '3',
+    description: 'Hungary, Pech',
+    createdAt: new Date(2017, 09, 26, 16, 05),
+    author: 'Mary',
+    photoLink: '3.jpg',
+    likes: ['ChesnAnastasia'],
+    tags: ['#travel', '#beauty', '#happiness']
+}
+var post4 = {
+    id: '4',
+    description: 'Hungary, Pech',
+    createdAt: new Date(2017, 09, 26, 18, 24),
+    author: 'ChesnAnastasia',
+    photoLink: '2.jpg',
+    likes: ['Mary', 'Kate'],
+    tags: ['#happiness', '#travel']
+}
+var post5 = {
+    id: '5',
+    description: 'Hungary, Pech',
+    createdAt: new Date(2017, 09, 26, 16, 52),
+    author: 'Kate',
+    photoLink: '1.jpg',
+    likes: ['ChesnAnastasia', 'Mary'],
+    tags: ['#happiness', '#beauty']
+}
+
+var options = {
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+};
+let module = (function () {
+
+    let user = null;
+    let User = document.querySelector('.User');
+    let username = document.querySelector('.user-name');
+    let tape = document.querySelector('.Tape');
+    let editProfile = document.querySelector('.b1');
+    let addPost = document.querySelector('.b2');
+    let logOut = document.querySelector('.b3');
+    let logIn = document.querySelector('.b4');
+    let imgProfile = document.querySelector('.img-profile');
+
+    function profile(b1, b2, b3, b4, img, i) {
+        editProfile.style.display = b1;
+        addPost.style.display = b2;
+        logOut.style.display = b3;
+        logIn.style.display = b4;
+        imgProfile.style.display = img;
+    }
+
+    let changeUser = function (newUser) {
+        if (newUser !== null && typeof newUser === 'string' && newUser !== '') {
+            this.user = newUser;
+            username.innerHTML = this.user;
+            profile('', '', '', 'none', '');
+            User.style.height = '140px';
+            tape.style.margin = '190px 0 0 0';
+        } else {
+            this.user = null;
+            username.innerHTML = 'Guest';
+            profile('none', 'none', 'none', '', 'none');
+            User.style.height = '50px';
+            tape.style.margin = '100px 0 0 0';
+        }
+    }
+
+    let createPhotoPost = function (post) {
+        var classes = document.getElementsByClassName("Tape");
+        let myclass = classes[0];
+        let newPost = document.createElement('div');
+        newPost.id = post.id;
+        newPost.className = 'post';
+        newPost.innerHTML = '<img class="post-photo" src="' + post.photoLink + '" alt="photo"><div class="info-about-post"><div class="post-author-tags-description">' +
+            '<p>' + post.author + '</p><p class="post-tags">' + post.tags + '</p><p class="post-description">' + post.description + '</p></div>' +
+            '<div class="date-time-icons"><div class="icons-block"><i class="like-icon material-icons">favorite_border</i>' +
+            '<i class="edit-icon material-icons">mode_edit</i><i class="delete-icon material-icons">delete</i></div>' +
+            '<button class="count-likes">Show ' + post.likes.length + ' likes</button>' +
+            '<p>' + post.createdAt.toLocaleString("ru", options) + '</p></div></div>';
+        myclass.appendChild(newPost);
+        return newPost;
+    }
+    let addPhotoPost = function (photoPost) {
+        if (moduleF.addPhotoPost(photoPost)) {
+            createPhotoPost(photoPost);
+            return true;
+        } else return false;
+    }
+
+    let clearTape = function () {
+        tape.innerHTML = '';
+    }
+
+    let getPhotoPosts = function (skip, top, filterConfig) {
+        let posts = moduleF.getPhotoPosts(skip, top, filterConfig);
+        posts.forEach(item => {
+            tape.appendChild(createPhotoPost(item));
+        });
+    }
+
+    let removePhotoPost = function (id) {
+        if (moduleF.removePhotoPost(id)) {
+            tape.removeChild(document.getElementById(id));
+            return true;
+        }
+        else return false;
+    }
+
+    let editPhotoPost = function (id, photoPost) {
+        if (moduleF.editPhotoPost(id, photoPost)) {
+            tape.replaceChild(createPhotoPost(moduleF.getPhotoPost(id)), document.getElementById(id));
+            return true;
+        }
+        return false;
+    }
+
+    return {
+        user,
+        changeUser,
+        addPhotoPost,
+        clearTape,
+        getPhotoPosts,
+        removePhotoPost,
+        editPhotoPost
+
+    }
+})();
+
+module.changeUser();
+module.changeUser('ChesnAnastasia');
+
+module.addPhotoPost(post1);
+module.addPhotoPost(post2);
+module.addPhotoPost(post3);
+module.addPhotoPost(post4);
+module.addPhotoPost(post5);
+
+module.removePhotoPost('1');
+
+module.editPhotoPost('2', { tags: ['#travel', '#beauty', '#positive'] });
+
+module.clearTape();
+module.getPhotoPosts(1, 3);
+module.clearTape();
+module.getPhotoPosts(0, photoPosts.length);
+
